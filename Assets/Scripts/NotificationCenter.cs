e789805002ab412f9439f85cd8e2cef365a60589
NANORAGE\Nano Rage <rayagordo@gmail.com>
2017-01-07 12:14:06 +0100

    Prueba

--- Assets/Scripts/NotificationCenter.cs
+++ Assets/Scripts/NotificationCenter.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
  
-//    NotificationCenter is used for handling messages between GameObjects.
+
+//    illo
 //    GameObjects can register to receive specific notifications.  When another objects sends a notification of that type, all GameObjects that registered for it and implement the appropriate message will receive that notification.
 //    Observing GameObjects must register to receive notifications with the AddObserver function, and pass their selves, and the name of the notification.  Observing GameObjects can also unregister themselves with the RemoveObserver function.  GameObjects must request to receive and remove notification types on a type by type basis.
 //    Posting notifications is done by creating a Notification object and passing it to PostNotification.  All receiving GameObjects will accept that Notification object.  The Notification object contains the sender, the notification type name, and an option hashtable containing data.
